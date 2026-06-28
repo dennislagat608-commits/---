1064ac5ccfc8c27824945c1bd3c0ea98fe0e2a98
dennislagat608-commits <dennislagat608@gmail.com>
2026-06-29 01:54:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,25 @@
-conf
+
+### **9. Example Commands in `/commands/`**
+
+#### `/commands/general/menu.js`
+```javascript
+import config from '../../config.js';
+export default {
+  name: 'menu',
+  alias: ['help'],
+  run: async({sock, m, reply}) => {
+    const txt = `*${config.BOT_NAME} V${config.VERSION}*
+
+*GENERAL* /start /help /ping /version
+*FONTS* /bold /italic /zalgo /random
+*AI* /gpt /gptimg /gptclear
+*GROUP* /kick /ban /mute /warn /promote
+*DOWNLOAD* /ytmp3 /ytmp4 /tiktok /ig
+*TOOLS* /weather /translate /qr /sticker
+*FUN* /meme /8ball /dice /trivia
+*DATING* /marry /waifu /match /rizz
+
+Total: 200+ cmds. Type /cmdname to use.`;
+    reply(txt);
+  }
+}
